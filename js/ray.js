03fc3555e3bc80bf9ab34fd044b1998cd9ce308c
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2024-12-08 07:40:13 -0300

    Função de Calculo da posição de intercessão, calculo da menor distancia e correção no angulo dos raios

--- js/ray.js
+++ js/ray.js
@@ -1,51 +1,63 @@
-class Ray{
-    constructor(x,y){
-        this.pos = {
-            x:x,
-            y:y
-        };
+class Ray {
+	constructor(x, y, angle) {
+		this.pos = {
+			x: x,
+			y: y,
+		};
+
+        // Convert angle from degrees to radians
+        var radians = angle * (Math.PI / 180);
+
+        var dirX = Math.cos(radians);
+        var dirY = Math.sin(radians);
+
         this.dir = {
-            x:1,
-            y:0
+            x: dirX,
+            y: dirY
         };
 
-        this.length = 30;
-    }
+		this.length = 10;
+	}
 
-    show(){
-        ctx.beginPath();
-        ctx.strokeStyle = "white";
-        ctx.moveTo(this.pos.x, this.pos.y);
-        ctx.lineTo(this.pos.x + this.dir.x * this.length, this.pos.y + this.dir.y * this.length);
-        ctx.stroke();
-    }
+	show() {
+		ctx.beginPath();
+		ctx.strokeStyle = "white";
+		ctx.moveTo(this.pos.x, this.pos.y);
+		ctx.lineTo(this.pos.x + this.dir.x * this.length, this.pos.y + this.dir.y * this.length);
+		ctx.stroke();
+	}
 
+	cast(wall) {
+		const x1 = wall.start.x;
+		const y1 = wall.start.y;
+		const x2 = wall.end.x;
+		const y2 = wall.end.y;
 
-    cast(wall){
-        const x1 = wall.start.x;
-        const y1 = wall.start.y;
-        const x2 = wall.end.x;
-        const y2 = wall.end.y;
+		const x3 = this.pos.x;
+		const y3 = this.pos.y;
+		const x4 = this.pos.x + this.dir.x * this.length;
+		const y4 = this.pos.y + this.dir.y * this.length;
 
-        const x3 = this.pos.x;
-        const y3 = this.pos.y;
-        const x4 = this.pos.x + (this.dir.x * this.length);
-        const y4 = this.pos.y + (this.dir.y * this.length);
+		const denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+		if (denominator == 0) {
+			return false;
+		}
 
-        const denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-        if (denominator == 0) {
+		var t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denominator;
+		var u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denominator;
+
+        if (t > 0 && t < 1 && u > 0) {
+            var x = x1 + t * (x2 - x1);
+            var y = y1 + t * (y2 - y1);
+
+            return { x: x, y: y };
+        } else {
             return false;
-        }
+        } 
+	}
 
-
-        var t = ((x1 - x3) * (y3 * y4) - (y1-y3) * (x3-x4)) / denominator;
-        var u = (x1 - x2)* (y1-y1) - (y1-)
-
-        
+    move(x,y){
+        this.pos.x = x;
+        this.pos.y = y;
     }
-
-
-
-
-
 }
--- js/script.js
+++ js/script.js
@@ -2,15 +2,27 @@ const ctx = $("#canvas")[0].getContext("2d");
 var walls = [];
 var rays = [];
 
-
 const init = ()=>{
+    walls.push(new Wall(0,0,600,0));
+    walls.push(new Wall(600,0,600,600));
+    walls.push(new Wall(600,600,0,600));
+    walls.push(new Wall(0,600,0,0));
+
+
+    for (let i = 0; i < 4; i++) {
+        walls.push(new Wall(Math.random() * 600, Math.random() * 600, Math.random() * 600, Math.random() * 600));
+        
+    }
+
     walls.push(new Wall(300, 100, 300, 300));
-    rays.push(new Ray(100,200));
+
+    for (let i = 0; i < 360; i+=1) {
+        rays.push(new Ray(100,200,i));
+    }
 }
 
-
-
 function draw(){
+    ctx.clearRect(0,0,600,600);
     for (const wall of walls){
         wall.show();        
     }
@@ -19,19 +31,45 @@ function draw(){
         ray.show();
     }
 
-
     checkIntersections();
-
+    requestAnimationFrame(draw);
 }
 
 function checkIntersections(){
     for (const ray of rays){
+        var closest = null;
+        var min = Infinity;
         for (const wall of walls){
-            ray.cast(wall);
+            var intercept = ray.cast(wall);
+            if (intercept){
+                var distancia = Math.sqrt(Math.pow(ray.pos.x - intercept.x, 2) + Math.pow(ray.pos.y - intercept.y, 2));
+                
+                if (distancia < min){
+                    min = distancia;
+                    closest = intercept;
+                }
+            }
+        }
+        if (closest) {
+            ctx.beginPath();
+                ctx.strokeStyle = "white";
+                ctx.moveTo(ray.pos.x, ray.pos.y);
+                ctx.lineTo(closest.x, closest.y);
+                ctx.stroke();
         }
     }
 }
 
+$(document).ready(()=>{
+    init();
+    requestAnimationFrame(draw);
 
-init();
-draw();
+
+    $(document).on("mousemove", (e)=>{
+        for (const ray of rays){
+            ray.move(e.clientX, e.clientY);
+        }
+
+        
+    });
+});
