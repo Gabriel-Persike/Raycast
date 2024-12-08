f83ad0086ed853482155c475766480e2d269755d
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2024-12-08 06:35:04 -0300

    Update cast

--- js/ray.js
+++ js/ray.js
@@ -31,6 +31,17 @@ class Ray{
         const y3 = this.pos.y;
         const x4 = this.pos.x + (this.dir.x * this.length);
         const y4 = this.pos.y + (this.dir.y * this.length);
+
+        const denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (denominator == 0) {
+            return false;
+        }
+
+
+        var t = ((x1 - x3) * (y3 * y4) - (y1-y3) * (x3-x4)) / denominator;
+        var u = (x1 - x2)* (y1-y1) - (y1-)
+
+        
     }
 
 
